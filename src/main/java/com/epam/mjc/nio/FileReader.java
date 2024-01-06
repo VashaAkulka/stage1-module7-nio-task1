0a408ef671aaa3f6ada79bbb64e99d7c2c56e9ea
Виктор <eee@mail.ru>
2024-01-06 15:40:04 +0300

    yes

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String input;
+        try {
+            input = Files.readString(Path.of(file.getPath()));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String[] values = new String[4];
+        String[] lines = input.split("\r\n");
+        int i = 0;
+        for (String line : lines) {
+            int index = line.indexOf(':');
+            values[i++] = line.substring(index + 1).trim();
+        }
+
+        return new Profile(values[0], Integer.valueOf(values[1]), values[2], Long.valueOf(values[3]));
     }
 }
